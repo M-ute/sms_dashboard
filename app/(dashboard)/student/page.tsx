03e819e638b7e1bb3138cfa83424fc5da63ac625
AMOAH BENJAMIN <amoahbenjamin@AMOAHs-MacBook-Pro.local>
2026-04-17 15:22:05 +0000

    student schedule

--- app/(dashboard)/student/page.tsx
+++ app/(dashboard)/student/page.tsx
@@ -1,7 +1,30 @@
+"use client"
+
+import Announcements from "@/components/Announcements"
+import BigCalendar from "@/components/BigCalender"
+import Calender from "@/components/Calender"
+
+
 const StudentPage = () => {
   return (
-    <div className=''>
+    <div className='p-4 flex gap-4 flex-col xl:flex-row'>
+      {/* LEFT */}
+      <div className="w-full xl:w-2/3">
+      <div className="h-full bg-white p-4 text-black rounded-md">
+        <h1 className="text-xl font-semibold">Schedule (4A)</h1>
+        <BigCalendar/>
+      </div>
+
+      </div>
+
+
+      {/* RIGHT */}
+      <div className="w-full xl:w-1/3 text-black flex-col gap-8 "> 
+        <Calender/>
+        
+        <Announcements/>
       
+      </div>
     </div>
   )
 }
