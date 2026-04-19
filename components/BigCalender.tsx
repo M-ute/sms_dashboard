1917395f4ea5e2403e47d08524690fe8bb8f251a
AMOAH BENJAMIN <amoahbenjamin@AMOAHs-MacBook-Pro.local>
2026-04-19 01:22:29 +0000

    removed date controller

--- components/BigCalender.tsx
+++ components/BigCalender.tsx
@@ -1,20 +1,35 @@
-import { Calendar, momentLocalizer } from 'react-big-calendar'
+"use client"
+
+import { Calendar, momentLocalizer, View, Views } from 'react-big-calendar'
 import moment from 'moment'
 import { calendarEvents } from '@/app/lib/data';
 import "react-big-calendar/lib/css/react-big-calendar.css"
+import { useState } from 'react';
 
-const localizer = momentLocalizer(moment)
+const localizer = momentLocalizer(moment);
 
-const BigCalendar = () => (
-  <div>
-    <Calendar
-      localizer={localizer}
-      events={calendarEvents}
-      startAccessor="start"
-      endAccessor="end"
-      style={{ height: 500 }}
-    />
-  </div>
-);
+
+const BigCalendar = () => {
+    const [view, setView] = useState<View>(Views.WORK_WEEK);
+
+    const handleOnChangeView = (selectedView:View) => {
+        setView(selectedView);
+    }
+
+    return(
+        <Calendar
+        localizer={localizer}
+        events={calendarEvents}
+        startAccessor="start"
+        endAccessor="end"
+        views={["work_week", "day"]}
+        view={view}
+        style={{ height: "98%" }}
+        onView={handleOnChangeView}
+        min={new Date (2026,1,0,8,0,0)}
+        max={new Date (2026,1,0,18,0,0)} 
+        />
+    );
+};
 
 export default BigCalendar;
